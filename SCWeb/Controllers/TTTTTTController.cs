92d00f2af105aae0a4e4ab3518ce69634b023b3b
If I Changed <274895086@qq.com>
2019-11-25 13:47:02 +0800

    0

---
+++ SCWeb/Controllers/TTTTTTController.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using SCWeb.Models;
+using SqlSugar;
+
+namespace SCWeb.Controllers
+{
+    public class TTTTTTController : BaseController
+    {
+        // GET: TTTTTT
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Edit()
+        {
+            return View();
+        }
+        public ActionResult EDitIndex(string spdm,string bd,string jijie)
+        {
+            //var t17 = db.Updateable<BS_BUS_Samples>().UpdateColumns(it =>
+            //new BS_BUS_Samples()
+            //{
+            //    StageName = SqlFunc.Subqueryable<School>().Where(s => s.Id == it.SchoolId).Select(s => s.Id),
+            //    Name = "newname"
+            //}).Where(it => it.Id == 1).ExecuteCommand();
+            var  bddm = BBdm(bd);
+            var jjdm = 0;
+            switch (jijie)
+            {
+                case "春季":
+                    jjdm = 1;
+                    break;
+                case "夏季":
+                    jjdm =2;
+                    break;
+                case "秋季":
+                    jjdm = 3;
+                    break;
+                case "冬季":
+                    jjdm = 4;
+                    break;
+            }
+            try
+            {
+                db.Ado.BeginTran();
+                if (db.Updateable<BS_BUS_Samples>(new { SeasonName = jijie, SeasonCode = jjdm, Property02 = bd, PrprCode02 = bddm })
+                    .Where(u=>u.Code==spdm).ExecuteCommand() > 0)
+                {
+                    if (db.Updateable<BS_BUS_ColorBOMMaster>(new { SeasonName = jijie, SeasonCode = jjdm, StageName = bd, StageCode = bddm })
+                      .Where(u => u.GoodsCode == spdm).ExecuteCommand() > 0)
+                    {
+                        return Content("y");
+                    };
+                }
+                 
+            }
+            catch (Exception ex)
+            {
+                db.Ado.RollbackTran();
+                throw ex;
+            }
+            return Content("n");
+        }
+        
+        public async Task<JsonResult> List()
+        {
+            var page = int.Parse(Request["page"] ?? "1");
+            var limit = int.Parse(Request["limit"] ?? "10");
+            var year = Request["year"].ToString().Trim();
+            var jijie = Request["jijie"].ToString().Trim();
+            var namespdm = Request["namespdm"].ToString().Trim();
+            var list = await db.Queryable<BS_BUS_Samples>()
+            .Where(s => SqlFunc.ToInt32(s.YearCode) >= 2020)
+            .WhereIF(!string.IsNullOrEmpty(year),s=>s.YearCode==year)
+            .WhereIF(!string.IsNullOrEmpty(jijie),s => s.SeasonName == jijie)
+            .WhereIF(!string.IsNullOrEmpty(namespdm), s => SqlFunc.StartsWith(s.Code,namespdm) || SqlFunc.EndsWith(s.Code, namespdm))
+            .Select((s) => new
+            {
+                s.YearCode,
+                s.SeasonName,
+                s.Property02,
+                s.Code,
+                s.BigTypeName,
+                s.Name
+            }).ToListAsync();
+            return Json(new { code = 0, data = list.Skip((page - 1) * limit).Take(limit), count=list.Count()}, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ SCWeb/Models/BS_BUS_ColorBOMMaster.cs
@@ -0,0 +1,350 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SCWeb.Models
+{
+    public partial class BS_BUS_ColorBOMMaster
+    {
+        public BS_BUS_ColorBOMMaster()
+        {
+        }
+
+        private System.String _MasterID;
+        /// <summary>
+        /// 
+        /// </summary>
+        public System.String MasterID { get { return this._MasterID; } set { this._MasterID = value; } }
+
+        private System.Int64 _FID;
+        /// <summary>
+        /// 
+        /// </summary>
+        public System.Int64 FID { get { return this._FID; } set { this._FID = value; } }
+
+        private System.String _OperatorID;
+        /// <summary>
+        /// 
+        /// </summary>
+        public System.String OperatorID { get { return this._OperatorID; } set { this._OperatorID = value; } }
+
+        private System.String _Operator;
+        /// <summary>
+        /// 
+        /// </summary>
+        public System.String Operator { get { return this._Operator; } set { this._Operator = value; } }
+
+        private System.DateTime? _CreateTime;
+        /// <summary>
+        /// 
+        /// </summary>
+        public System.DateTime? CreateTime { get { return this._CreateTime; } set { this._CreateTime = value; } }
+
+        private System.DateTime? _UpdateTime;
+        /// <summary>
+        /// 
+        /// </summary>
+        public System.DateTime? UpdateTime { get { return this._UpdateTime; } set { this._UpdateTime = value; } }
+
+        private System.DateTime? _AuditTime;
+        /// <summary>
+        /// 
+        /// </summary>
+        public System.DateTime? AuditTime { get { return this._AuditTime; } set { this._AuditTime = value; } }
+
+        private System.Byte[] _LastUpdateTime;
+        /// <summary>
+        /// 
+        /// </summary>
+        public System.Byte[] LastUpdateTime { get { return this._LastUpdateTime; } set { this._LastUpdateTime = value; } }
+
+        private System.Int32? _State;
+        /// <summary>
+        /// 
+        /// </summary>
+        public System.Int32? State { get { return this._State; } set { this._State = value; } }
+
+        private System.String _Remark;
+        /// <summary>
+        /// 
+        /// </summary>
+        public System.String Remark { get { return this._Remark; } set { this._Remark = value; } }
+
+        private System.String _Code;
+        /// <summary>
+        /// 
+        /// </summary>
+        public System.String Code { get { return this._Code; } set { this._Code = value; } }
+
+        private System.String _Name;
+        /// <summary>
+        /// 
+        /// </summary>
+        public System.String Name { get { return this._Name; } set { this._Name = value; } }
+
+        private System.String _ParentID;
+        /// <summary>
+        /// 
+        /// </summary>
+        public System.String ParentID { get { return this._ParentID; } set { this._ParentID = value; } }
+
+        private System.String _ParentCode;
+        /// <summary>
+        /// 
+        /// </summary>
+        public System.String ParentCode { get { return this._ParentCode; } set { this._ParentCode = value; } }
+
+        private System.String _GoodsID;
+        /// <summary>
+        /// 
+        /// </summary>
+        public System.String GoodsID { get { return this._GoodsID; } set { this._GoodsID = value; } }
+
+        private System.String _GoodsCode;
+        /// <summary>
+        /// 
+        /// </summary>
+        public System.String GoodsCode { get { return this._GoodsCode; } set { this._GoodsCode = value; } }
+
+        private System.String _GoodsName;
+        /// <summary>
+        /// 
+        /// </summary>
+        public System.String GoodsName { get { return this._GoodsName; } set { this._GoodsName = value; } }
+
+        private System.String _ColorCode;
+        /// <summary>
+        /// 
+        /// </summary>
+        public System.String ColorCode { get { return this._ColorCode; } set { this._ColorCode = value; } }
+
+        private System.String _ColorName;
+        /// <summary>
+        /// 
+        /// </summary>
+        public System.String ColorName { get { return this._ColorName; } set { this._ColorName = value; } }
+
+        private System.String _SizeCode;
+        /// <summary>
+        /// 
+        /// </summary>
+        public System.String SizeCode { get { return this._SizeCode; } set { this._SizeCode = value; } }
+
+        private System.String _SizeName;
+        /// <summary>
+        /// 
+        /// </summary>
+        public System.String SizeName { get { return this._SizeName; } set { this._SizeName = value; } }
+
+        private System.String _SeasonCode;
+        /// <summary>
+        /// 
+        /// </summary>
+        public System.String SeasonCode { get { return this._SeasonCode; } set { this._SeasonCode = value; } }
+
+        private System.String _SeasonName;
+        /// <summary>
+        /// 
+        /// </summary>
+        public System.String SeasonName { get { return this._SeasonName; } set { this._SeasonName = value; } }
+
+        private System.String _BrandCode;
+        /// <summary>
+        /// 
+        /// </summary>
+        public System.String BrandCode { get { return this._BrandCode; } set { this._BrandCode = value; } }
+
+        private System.String _BrandName;
+        /// <summary>
+        /// 
+        /// </summary>
+        public System.String BrandName { get { return this._BrandName; } set { this._BrandName = value; } }
+
+        private System.String _Designer;
+        /// <summary>
+        /// 
+        /// </summary>
+        public System.String Designer { get { return this._Designer; } set { this._Designer = value; } }
+
+        private System.String _DesignID;
+        /// <summary>
+        /// 
+        /// </summary>
+        public System.String DesignID { get { return this._DesignID; } set { this._DesignID = value; } }
+
+        private System.String _BigType;
+        /// <summary>
+        /// 
+        /// </summary>
+        public System.String BigType { get { return this._BigType; } set { this._BigType = value; } }
+
+        private System.String _BigTypeName;
+        /// <summary>
+        /// 
+        /// </summary>
+        public System.String BigTypeName { get { return this._BigTypeName; } set { this._BigTypeName = value; } }
+
+        private System.String _ModelCode;
+        /// <summary>
+        /// 
+        /// </summary>
+        public System.String ModelCode { get { return this._ModelCode; } set { this._ModelCode = value; } }
+
+        private System.String _ModelName;
+        /// <summary>
+        /// 
+        /// </summary>
+        public System.String ModelName { get { return this._ModelName; } set { this._ModelName = value; } }
+
+        private System.String _Auditor;
+        /// <summary>
+        /// 
+        /// </summary>
+        public System.String Auditor { get { return this._Auditor; } set { this._Auditor = value; } }
+
+        private System.String _CfgModelCode;
+        /// <summary>
+        /// 
+        /// </summary>
+        public System.String CfgModelCode { get { return this._CfgModelCode; } set { this._CfgModelCode = value; } }
+
+        private System.String _CfgModelName;
+        /// <summary>
+        /// 
+        /// </summary>
+        public System.String CfgModelName { get { return this._CfgModelName; } set { this._CfgModelName = value; } }
+
+        private System.String _ParentSelfID;
+        /// <summary>
+        /// 
+        /// </summary>
+        public System.String ParentSelfID { get { return this._ParentSelfID; } set { this._ParentSelfID = value; } }
+
+        private System.String _ParentSelfCode;
+        /// <summary>
+        /// 
+        /// </summary>
+        public System.String ParentSelfCode { get { return this._ParentSelfCode; } set { this._ParentSelfCode = value; } }
+
+        private System.String _SimpleName;
+        /// <summary>
+        /// 
+        /// </summary>
+        public System.String SimpleName { get { return this._SimpleName; } set { this._SimpleName = value; } }
+
+        private System.Int32? _AuditImk;
+        /// <summary>
+        /// 
+        /// </summary>
+        public System.Int32? AuditImk { get { return this._AuditImk; } set { this._AuditImk = value; } }
+
+        private System.Int32? _LocArtNote;
+        /// <summary>
+        /// 
+        /// </summary>
+        public System.Int32? LocArtNote { get { return this._LocArtNote; } set { this._LocArtNote = value; } }
+
+        private System.String _ImpArtNote;
+        /// <summary>
+        /// 
+        /// </summary>
+        public System.String ImpArtNote { get { return this._ImpArtNote; } set { this._ImpArtNote = value; } }
+
+        private System.String _ImpArtReMark;
+        /// <summary>
+        /// 
+        /// </summary>
+        public System.String ImpArtReMark { get { return this._ImpArtReMark; } set { this._ImpArtReMark = value; } }
+
+        private System.String _ConfigMaterTypeCode;
+        /// <summary>
+        /// 
+        /// </summary>
+        public System.String ConfigMaterTypeCode { get { return this._ConfigMaterTypeCode; } set { this._ConfigMaterTypeCode = value; } }
+
+        private System.String _ConfigMaterTypeName;
+        /// <summary>
+        /// 
+        /// </summary>
+        public System.String ConfigMaterTypeName { get { return this._ConfigMaterTypeName; } set { this._ConfigMaterTypeName = value; } }
+
+        private System.String _StageCode;
+        /// <summary>
+        /// 
+        /// </summary>
+        public System.String StageCode { get { return this._StageCode; } set { this._StageCode = value; } }
+
+        private System.String _StageName;
+        /// <summary>
+        /// 
+        /// </summary>
+        public System.String StageName { get { return this._StageName; } set { this._StageName = value; } }
+
+        private System.String _ArrangeMaiState;
+        /// <summary>
+        /// 
+        /// </summary>
+        public System.String ArrangeMaiState { get { return this._ArrangeMaiState; } set { this._ArrangeMaiState = value; } }
+
+        private System.String _ContactID;
+        /// <summary>
+        /// 
+        /// </summary>
+        public System.String ContactID { get { return this._ContactID; } set { this._ContactID = value; } }
+
+        private System.String _ContactCode;
+        /// <summary>
+        /// 
+        /// </summary>
+        public System.String ContactCode { get { return this._ContactCode; } set { this._ContactCode = value; } }
+
+        private System.String _TailorCode;
+        /// <summary>
+        /// 
+        /// </summary>
+        public System.String TailorCode { get { return this._TailorCode; } set { this._TailorCode = value; } }
+
+        private System.String _TailorName;
+        /// <summary>
+        /// 
+        /// </summary>
+        public System.String TailorName { get { return this._TailorName; } set { this._TailorName = value; } }
+
+        private System.String _SampleDevCode;
+        /// <summary>
+        /// 
+        /// </summary>
+        public System.String SampleDevCode { get { return this._SampleDevCode; } set { this._SampleDevCode = value; } }
+
+        private System.String _SampleDevName;
+        /// <summary>
+        /// 
+        /// </summary>
+        public System.String SampleDevName { get { return this._SampleDevName; } set { this._SampleDevName = value; } }
+
+        private System.String _BoardDesigner;
+        /// <summary>
+        /// 
+        /// </summary>
+        public System.String BoardDesigner { get { return this._BoardDesigner; } set { this._BoardDesigner = value; } }
+
+        private System.String _BoardDesignID;
+        /// <summary>
+        /// 
+        /// </summary>
+        public System.String BoardDesignID { get { return this._BoardDesignID; } set { this._BoardDesignID = value; } }
+
+        private System.String _ContactBillID;
+        /// <summary>
+        /// 
+        /// </summary>
+        public System.String ContactBillID { get { return this._ContactBillID; } set { this._ContactBillID = value; } }
+
+        private System.String _ContactBillCode;
+        /// <summary>
+        /// 
+        /// </summary>
+        public System.String ContactBillCode { get { return this._ContactBillCode; } set { this._ContactBillCode = value; } }
+    }
+}
